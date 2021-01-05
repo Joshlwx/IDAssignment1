7a4172cba15e531a4699d011db303f605ea97418
Joshua Lim <60908729+Joshlwx@users.noreply.github.com>
2021-01-05 12:26:29 +0800

    Add files via upload

--- contact.js
+++ contact.js
@@ -2,7 +2,7 @@ window.onload = setForm;
 
 function setForm() {
    document.forms[0].onsubmit = function() {
-      if (this.checkValidity()) alert("Your form have been sucessfully submitted.");
+      if (this.checkValidity()) alert("Your form have been submitted sucessfully.");
       return false;
     }
 }
